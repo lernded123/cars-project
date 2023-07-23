5e74ba493bb8ecb88e30e488ddb443459087335a
Sams <goodlifesam@gmail.com>
2023-07-23 13:04:22 -0700

    mongo

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express =require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
 
 app.use(bodyParser.urlencoded({extended: true}))
 
