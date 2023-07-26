57122a2d811eb3cf0992f6a9163808906746a931
Sams <goodlifesam@gmail.com>
2023-07-26 12:48:55 -0700

    server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express =require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
-app.use(bodyParser.urlencoded({}))
+app.use(bodyParser.urlencoded({extended: true}))
 
 
 app.get('/',(request,response) => {
