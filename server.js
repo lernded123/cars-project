efbbccc9a096a206b60b55df8d1785ba188055d1
Sams <goodlifesam@gmail.com>
2023-09-19 12:30:24 -0700

    html

---
+++ server.js
@@ -0,0 +1,16 @@
+const express =require('express')
+const app = express()
+
+app.listen(3000, function(){
+    console.log('listening on 3000');
+})
+
+
+app.get('/',(request,response) => {
+  response.sendFile(__dirname + '/index.html')
+    
+})
+
+app.post('/quotes', (req,res) => {
+    console.log('Something');
+})
